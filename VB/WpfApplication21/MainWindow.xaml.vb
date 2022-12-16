2ea49ddc7c24c61437b15cbc67c9ea7a91b1dc0c
DevExpressExampleBot <fakeEmail>
2022-12-16 23:11:31 +0400

    Source auto update [skip ci]

--- VB/WpfApplication21/MainWindow.xaml.vb
+++ VB/WpfApplication21/MainWindow.xaml.vb
@@ -1,22 +1,23 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 
 Namespace WpfApplication21
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			InitSources()
-		End Sub
 
-		Private Sub InitSources()
-			Dim source As New ProductList()
-			listBoxEdit1.ItemsSource = source
-			checkedListBoxEdit.ItemsSource = source
-			radioListBoxEdit.ItemsSource = source
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            InitSources()
+        End Sub
+
+        Private Sub InitSources()
+            Dim source As ProductList = New ProductList()
+            Me.listBoxEdit1.ItemsSource = source
+            Me.checkedListBoxEdit.ItemsSource = source
+            Me.radioListBoxEdit.ItemsSource = source
+        End Sub
+    End Class
 End Namespace
